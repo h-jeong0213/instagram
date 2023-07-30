3bb13b122210868bd4afb3529c278906c3af8775
York_Ki <p.ryan9311@gmail.com>
2023-07-30 16:35:48 +0900

    이건 또 무슨 에러지...

---
+++ src/pages/signUp/index.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+const SignUpPage = () => {
+  return (
+    <>
+      <div className="container">
+        {/* <image className="logo"></image> */}
+        <div>
+          <h1>
+            친구들의 사진과 동영상을 보려면 <br />
+            가입하세요
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUpPage;
