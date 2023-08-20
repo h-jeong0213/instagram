0377f89c88bf87fff2d4ba98732d6942c14fa712
York_Ki <p.ryan9311@gmail.com>
2023-08-20 20:11:15 +0900

    알림 클릭 시 사이드 nav 표시

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   Navbar,
   LogoBox,
@@ -10,66 +10,83 @@ import {
   MiniProfilImg,
   MoreCard,
   MoreCardItem,
-} from '../styles/nav.styles'
+  Alarm,
+} from "../styles/nav.styles";
 
 const NavMenu = () => {
-  let [more, setMore] = useState(false)
+  let [alarm, setAlarm] = useState(false);
+  let [more, setMore] = useState(false);
   return (
-    <Navbar>
-      <LogoBox>
-        <a>
-          <LogoImg src="../instagram.png"></LogoImg>
-        </a>
-      </LogoBox>
-      <NavList>
-        <NavItem>
-          <NavSvg className="navImgBox" src="house-heart-fill.svg"></NavSvg>
-          <NavText>홈</NavText>
+    <>
+      <Navbar>
+        <LogoBox>
+          <a>
+            <LogoImg src="../instagram.png"></LogoImg>
+          </a>
+        </LogoBox>
+        <NavList>
+          <NavItem>
+            <NavSvg className="navImgBox" src="house-heart-fill.svg"></NavSvg>
+            <NavText>홈</NavText>
+          </NavItem>
+          <NavItem>
+            <NavSvg className="navImgBox" src="search-heart-fill.svg"></NavSvg>
+            <NavText>검색</NavText>
+          </NavItem>
+          <NavItem>
+            <NavSvg className="navImgBox" src="compass.svg"></NavSvg>
+            <NavText>탐색</NavText>
+          </NavItem>
+          <NavItem>
+            <NavSvg
+              className="navImgBox"
+              src="chat-square-heart-fill.svg"
+            ></NavSvg>
+            <NavText>메세지</NavText>
+          </NavItem>
+          <NavItem
+            onClick={() => {
+              alarm == true ? setAlarm(!true) : setAlarm(true);
+            }}
+          >
+            <NavSvg className="navImgBox" src="heart (1).svg"></NavSvg>
+            <NavText>알림</NavText>
+          </NavItem>
+          <NavItem>
+            <NavSvg className="navImgBox" src="plus-square.svg"></NavSvg>
+            <NavText>만들기</NavText>
+          </NavItem>
+          <NavItem>
+            <MiniProfilImg className="navImgBox"></MiniProfilImg>
+            <NavText>프로필</NavText>
+          </NavItem>
+          {more == true ? (
+            <MoreCard>
+              <MoreCardItem>로그아웃</MoreCardItem>
+            </MoreCard>
+          ) : null}
+        </NavList>
+        <NavItem
+          className="moreBox"
+          onClick={() => {
+            more == true ? setMore(!true) : setMore(true);
+          }}
+        >
+          <NavSvg className="navImgBox" src="list (1).svg"></NavSvg>
+          <NavText>더 보기</NavText>
         </NavItem>
-        <NavItem>
-          <NavSvg className="navImgBox" src="search-heart-fill.svg"></NavSvg>
-          <NavText>검색</NavText>
-        </NavItem>
-        <NavItem>
-          <NavSvg className="navImgBox" src="compass.svg"></NavSvg>
-          <NavText>탐색</NavText>
-        </NavItem>
-        <NavItem>
-          <NavSvg
-            className="navImgBox"
-            src="chat-square-heart-fill.svg"
-          ></NavSvg>
-          <NavText>메세지</NavText>
-        </NavItem>
-        <NavItem>
-          <NavSvg className="navImgBox" src="heart (1).svg"></NavSvg>
-          <NavText>알림</NavText>
-        </NavItem>
-        <NavItem>
-          <NavSvg className="navImgBox" src="plus-square.svg"></NavSvg>
-          <NavText>만들기</NavText>
-        </NavItem>
-        <NavItem>
-          <MiniProfilImg className="navImgBox"></MiniProfilImg>
-          <NavText>프로필</NavText>
-        </NavItem>
-        {more == true ? (
-          <MoreCard>
-            <MoreCardItem>로그아웃</MoreCardItem>
-          </MoreCard>
-        ) : null}
-      </NavList>
-      <NavItem
-        className="moreBox"
-        onClick={() => {
-          more == true ? setMore(!true) : setMore(true)
-        }}
-      >
-        <NavSvg className="navImgBox" src="list (1).svg"></NavSvg>
-        <NavText>더 보기</NavText>
-      </NavItem>
-    </Navbar>
-  )
-}
+      </Navbar>
+      <Alarm isOpen={alarm}>
+        <h4>알림</h4>
+        <div>
+          <h5>이번 달</h5>
+        </div>
+        <div>
+          <h5>이전 활동</h5>
+        </div>
+      </Alarm>
+    </>
+  );
+};
 
-export default NavMenu
+export default NavMenu;
