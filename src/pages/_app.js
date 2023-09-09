ac87ba2dd8e81e636b7c13f46e64a4544a29c1a0
York_Ki <p.ryan9311@gmail.com>
2023-09-09 09:20:16 +0900

    createPool 생성

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
+import axios from "axios";
+import { createPool } from "mysql2/promise";
+import { createContext, useEffect, useState } from "react";
+
+export const UsersContext = createContext();
+
 export default function App({ Component, pageProps }) {
+  createPool({
+    host: "localhost",
+    user: restagramuser,
+    password: "1234",
+    database: "restagram",
+    port: 3306,
+  });
+  const [users, setUsers] = useState([]);
+
   return (
-    <>
+    <UsersContext>
       <Component {...pageProps} />
-    </>
+    </UsersContext>
   );
 }
