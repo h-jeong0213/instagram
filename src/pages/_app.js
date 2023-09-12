09c540fcee99e52940c445b937653324c843a742
h-jeong0213 <rhdwn3571@naver.com>
2023-09-12 13:42:36 +0900

    흠?...

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,39 @@
-import axios from "axios";
-import { createPool } from "mysql2/promise";
-import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import { createPool } from 'mysql2/promise'
+import { createContext, useEffect, useState } from 'react'
 
-export const UsersContext = createContext();
+export const UsersContext = createContext()
 
 export default function App({ Component, pageProps }) {
   createPool({
-    host: "localhost",
-    user: restagramuser,
-    password: "1234",
-    database: "restagram",
+    host: 'localhost',
+    user: 'restagramuser',
+    password: '1234',
+    database: 'restagram',
     port: 3306,
-  });
-  const [users, setUsers] = useState([]);
+  })
+  const [users, setUsers] = useState([])
+
+  //const mysqlData = ''
+
+  useEffect(() => {
+    const mysqlData = 'users.json'
+
+    axios
+      .get(mysqlData)
+      .then((users) => {
+        console.log(users.data)
+        setUsers(users.data)
+      })
+      .catch((err) => {
+        console.log('에러:', err)
+      })
+  }, [])
 
   return (
-    <UsersContext>
+    // value={{ users, setUsers }}
+    <UsersContext.Provider value={{ users, setUsers }}>
       <Component {...pageProps} />
-    </UsersContext>
-  );
+    </UsersContext.Provider>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
-import styled from "@emotion/styled";
-import axios from "axios";
+import styled from '@emotion/styled'
+import axios from 'axios'
+import { useContext } from 'react'
+import { UsersContext } from './_app'
 
 export default function Home() {
-  axios
-    .get("/users.json")
-    .then((users) => {
-      console.log(users.data);
-    })
-    .catch((err) => {
-      console.log("에러:", err);
-    });
+  // axios
+  //   .get('/users.json')
+  //   .then((users) => {
+  //     console.log(users.data)
+  //   })
+  //   .catch((err) => {
+  //     console.log('에러:', err)
+  //   })
+
+  const { users, setUsers } = useContext(UsersContext)
+  console.log(users)
+
   return (
     <>
       <h1>음..?</h1>
@@ -17,12 +23,12 @@ export default function Home() {
 
       <InputTest placeholder="댓글"></InputTest>
     </>
-  );
+  )
 }
 
-export const Container = styled.div``;
+export const Container = styled.div``
 
 export const InputTest = styled.input`
   outline: none;
   border: none;
-`;
+`
