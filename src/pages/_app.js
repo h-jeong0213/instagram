8adbd6cc47a53df23056abb4260d238a1b01453a
York_Ki <p.ryan9311@gmail.com>
2023-09-23 10:54:46 +0900

    재 커밋

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,4 @@
 export default function App({ Component, pageProps }) {
-
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
-}
-export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
